915cc162a83e66e28f830fc482a09616fa818c90
Hana <hanakeinan11@gmail.com>
2022-04-05 15:20:41 -0500

    Initial Commit

---
+++ SpaceNewsProject/SpaceNewsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsProjectApp.swift
+//  SpaceNewsProject
+//
+//  Created by Hana Keinan on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
